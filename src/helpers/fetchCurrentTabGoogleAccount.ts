eab07b303d87ede6559d57ce4c1781cffa689d55
ColeInc <chm@farmside.co.nz>
2023-09-06 11:15:09 +1200

    update classname to extract user email address from UI, google frontend update broke it

--- src/helpers/fetchCurrentTabGoogleAccount.ts
+++ src/helpers/fetchCurrentTabGoogleAccount.ts
@@ -1,24 +1,24 @@
 export const fetchCurrentTabGoogleAccount = (): string => {
-    try {
-        // Find the email address within the "gb_wc" div on google docs webpage:
-        const element = document.querySelector(".gb_wc");
+  try {
+    // Find the email address within the "gb_zc" div on google docs webpage:
+    const element = document.querySelector('.gb_zc');
 
-        if (element) {
-            const accountInfoDiv = element.querySelector("div:nth-child(3)");
+    if (element) {
+      const accountInfoDiv = element.querySelector('div:nth-child(3)');
 
-            if (accountInfoDiv) {
-                const email = accountInfoDiv.textContent?.trim() ?? "";
-                // console.log("Extracted dis email address:", email);
-                return email;
-            } else {
-                console.log('Email address div not found within "gb_wc" div :(');
-                return "";
-            }
-        } else {
-            return "";
-        }
-    } catch (error) {
-        console.log("failed to fetch user's email address", error);
-        return "";
+      if (accountInfoDiv) {
+        const email = accountInfoDiv.textContent?.trim() ?? '';
+        // console.log("Extracted dis email address:", email);
+        return email;
+      } else {
+        console.log('Email address div not found within "gb_wc" div :(');
+        return '';
+      }
+    } else {
+      return '';
     }
+  } catch (error) {
+    console.log("failed to fetch user's email address", error);
+    return '';
+  }
 };
